d370ecc191101532970a308a47f73ffae48ea7e1
CCYellowStar2 <149048350+CCYellowStar2@users.noreply.github.com>
2024-06-07 00:42:54 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -25,12 +25,12 @@ def search():
         q = q[:500]
         q = escape_ddg_bangs(q)
         region = request.args.get('region', 'wt-wt')
-        safesearch = request.args.get('safesearch', 'Off')
+        safesearch = request.args.get('safesearch', 'off')
         time = request.args.get('time', None)
         max_results = request.args.get('max_results', 3, type=int)
         max_results = min(max_results, 10)
 
-        results = DDGS().text(q, region=region, safesearch=safesearch, timelimit=time, max_results=max_results)
+        results = DDGS().text(q, region=region, safesearch=safesearch, max_results=max_results)
         print(results)
         response = jsonify(results)
         return response
